d967c2c6b36bdfa6cd37a044cc7cb3705438fa88
SvalovaNastya <Sun-7402@mail.ru>
2014-11-11 00:14:26 +0500

    Еще немного рефакторинга.

--- MarkParser/MarkParser/MarkParser/MarkParser.cs
+++ MarkParser/MarkParser/MarkParser/MarkParser.cs
@@ -7,31 +7,6 @@ using System.Threading.Tasks;
 
 namespace MarkToHtml
 {
-    public class Paragraph
-    {
-        public List<TaggedText> TaggedTextsList { get; set; }
-
-        public Paragraph(string rawText)
-        {
-            TaggedTextsList = ParseParagraph(rawText);
-        }
-
-        public List<TaggedText> ParseParagraph(string text)
-        {
-            return new List<TaggedText>(){new TaggedText(text, null)};
-        }
-
-        public string ToHtmlString()
-        {
-            var s = new StringBuilder();
-            s.Append("<p>\n");
-            foreach (var taggedText in TaggedTextsList)
-                s.Append(taggedText.ToHtmlString());
-            s.Append("\n</p>");
-            return s.ToString();
-        }
-    }
-
     public class MarkParser
     {
         public static string[] DivideIntoParagraphs(string text)
---
+++ MarkParser/MarkParser/MarkParser/Paragraph.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkToHtml
+{
+    public class Paragraph
+    {
+        public List<TaggedText> TaggedTextsList { get; set; }
+
+        public Paragraph(string rawText)
+        {
+            TaggedTextsList = ParseParagraph(rawText);
+        }
+
+        public List<TaggedText> ParseParagraph(string text)
+        {
+            return new List<TaggedText>() { new TaggedText(text, null) };
+        }
+
+        public string ToHtmlString()
+        {
+            var s = new StringBuilder();
+            s.Append("<p>\n");
+            foreach (var taggedText in TaggedTextsList)
+                s.Append(taggedText.ToHtmlString());
+            s.Append("\n</p>");
+            return s.ToString();
+        }
+    }
+}
